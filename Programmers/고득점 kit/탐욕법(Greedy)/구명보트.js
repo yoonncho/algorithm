23b70c0f7aa3dfdb7ad6de16f68e20ecd6379239
yoonncho <nicealice99@gmail.com>
2024-08-14 00:10:52 +0900

    [Programmers] 구명보트 코드 정리

--- Programmers/고득점 kit/탐욕법(Greedy)/구명보트.js
+++ Programmers/고득점 kit/탐욕법(Greedy)/구명보트.js
@@ -23,3 +23,25 @@ function solution(people, limit) {
   }
   return answer;
 }
+
+// 🌼 코드 정리
+function solution(people, limit) {
+  let answer = 0;
+  // 사람들의 몸무게를 내림차순으로 정렬
+  people.sort((a, b) => b - a);
+
+  // i는 가장 무거운 사람을, j는 가장 가벼운 사람을 가리킨다.
+  for (let i = 0, j = people.length - 1; i <= j; answer++) {
+    // 가장 무거운 사람과 가장 가벼운 사람을 함께 태울 수 있는지 확인
+    if (people[i] + people[j] <= limit) {
+      // 함께 태울 수 있다면, 두 사람 모두 보트에 태운다.
+      i++;
+      j--;
+    } else {
+      // 함께 태울 수 없다면, 무거운 사람만 보트에 태운다.
+      i++;
+    }
+  }
+
+  return answer; // 필요한 보트의 최소 개수를 반환
+}
