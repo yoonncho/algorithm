47b98c3be4a81f9fb4950ba8d26a84b6ad590e4b
yoonncho <nicealice99@gmail.com>
2024-08-18 22:32:39 +0900

    [Inflearn] 중복 순열

---
+++ Inflearn/Javascript/섹션8/[8]중복순열.js
@@ -0,0 +1,21 @@
+// 섹션8 - [8]: 중복 순열
+
+function solution(n, m) {
+  let answer = [];
+  let tmp = Array.from({ length: m }, () => 0);
+  function DFS(L) {
+    if (L === m) {
+      // tmp.slice로 얕은 복사
+      answer.push(tmp.slice());
+    } else {
+      for (let i = 1; i <= n; i++) {
+        tmp[L] = i;
+        DFS(L + 1);
+      }
+    }
+  }
+  DFS(0);
+  return answer;
+}
+
+console.log(solution(3, 2));
