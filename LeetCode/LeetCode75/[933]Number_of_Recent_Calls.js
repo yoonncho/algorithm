085919b4deeda83c48b3fe58725bda142e2dd46c
yoonncho <nicealice99@gmail.com>
2025-01-18 15:18:31 +0900

    [LeetCode75] Number of Recent Calls

---
+++ LeetCode/LeetCode75/[933]Number_of_Recent_Calls.js
@@ -0,0 +1,24 @@
+// Queue - 933. Number of Recent Calls
+// https://leetcode.com/problems/number-of-recent-calls/description/?envType=study-plan-v2&envId=leetcode-75
+
+var RecentCounter = function () {
+  this.q = [];
+};
+
+/**
+ * @param {number} t
+ * @return {number}
+ */
+RecentCounter.prototype.ping = function (t) {
+  this.q.push(t);
+  while (this.q[0] < t - 3000) {
+    this.q.shift();
+  }
+  return this.q.length;
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * var obj = new RecentCounter()
+ * var param_1 = obj.ping(t)
+ */
