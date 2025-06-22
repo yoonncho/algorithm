672d1dada68b06d6c3e32b03fda40ef512face1a
yoonncho <nicealice99@gmail.com>
2025-06-22 19:12:04 +0900

    SQL-[Programmers] ROOT 아이템 구하기

---
+++ SQL 고득점kit/ROOT_아이템_구하기.sql
@@ -0,0 +1,7 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/273710
+
+SELECT A.ITEM_ID, A.ITEM_NAME
+FROM ITEM_INFO AS A
+INNER JOIN ITEM_TREE AS B
+ON A.ITEM_ID = B.ITEM_ID
+WHERE B.PARENT_ITEM_ID IS NULL
