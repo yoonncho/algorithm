994324e14708dfaf2b9c803c7da84bf203b10269
yoonncho <nicealice99@gmail.com>
2025-06-22 16:20:43 +0900

    SQL-[Programmers] 흉부외과 또는 일반외과 의사 목록 출력하기

---
+++ SQL 고득점kit/흉부외과_또는_일반외과_의사_목록_출력하기.sql
@@ -0,0 +1,7 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/132203
+
+SELECT DR_NAME, DR_ID, MCDP_CD, DATE_FORMAT(HIRE_YMD, '%Y-%m-%d')
+FROM DOCTOR
+WHERE MCDP_CD IN ('CS', 'GS')
+-- MCDP_CD = "CS" OR MCDP_CD= "GS"
+ORDER BY HIRE_YMD DESC, DR_NAME ASC
