c24857b066f228ac3a6d72fcf96af24aac7a1a92
yoonncho <nicealice99@gmail.com>
2025-06-24 01:09:32 +0900

    SQL-[Programmers] 월별 잡은 물고기 수 구하기

---
+++ SQL 고득점kit/월별_잡은_물고기_수_구하기.sql
@@ -0,0 +1,7 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/293260
+
+SELECT COUNT(ID) AS FISH_COUNT, MONTH(TIME) AS MONTH
+FROM FISH_INFO
+GROUP BY MONTH(TIME)
+HAVING FISH_COUNT > 0
+ORDER BY MONTH
