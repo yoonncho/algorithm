6e011ef223769ae5feb6563fe2a5e87d7810899e
yoonncho <nicealice99@gmail.com>
2025-06-25 23:32:40 +0900

    SQL-[Programmers] 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기

---
+++ SQL 고득점kit/조회수가_가장_많은_중고거래_게시판의_첨부파일_조회하기.sql
@@ -0,0 +1,13 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/164671
+
+SELECT CONCAT('/home/grep/src/', BOARD_ID, '/', FILE_ID, FILE_NAME, FILE_EXT) as FILE_PATH
+FROM USED_GOODS_FILE
+WHERE BOARD_ID = (
+    SELECT BOARD_ID
+    FROM USED_GOODS_BOARD
+    ORDER BY VIEWS DESC
+    LIMIT 1
+)
+ORDER BY FILE_ID DESC
+
+
